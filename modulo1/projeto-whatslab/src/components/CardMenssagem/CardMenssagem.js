6c2955b1e134343820c5b99ee770ff6b06eae815
Victor Ramon <victoramonfm@gmail.com>
2022-06-03 14:11:04 -0300

    Projeto WhatsLab Final

--- modulo1/projeto-whatslab/src/components/CardMenssagem/CardMenssagem.js
+++ modulo1/projeto-whatslab/src/components/CardMenssagem/CardMenssagem.js
@@ -2,15 +2,23 @@ import React from "react";
 import styled from 'styled-components'
 
 const Mensagem = styled.div`
-border: thin solid black;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+gap: 5px;
+border: none;
 border-radius: 5px;
-margin: .3em;
+margin: 5px;
+width: 70%;
+padding: 1%  3%;
+background-color: #FFF;
 `
 
 function CardMensagem(props) {
+
     return (
         <Mensagem>
-            <p>{props.texto}</p>
+            <strong>{props.usuario}:</strong> {props.mensagem}
         </Mensagem>
     )
 }
