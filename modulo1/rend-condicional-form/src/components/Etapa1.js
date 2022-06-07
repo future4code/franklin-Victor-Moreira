f8d470c5750e0d4a5ad72e2c4c77989523f83892
Victor Ramon <victoramonfm@gmail.com>
2022-06-07 14:56:21 -0300

    Commit com correção da lista suspensa

--- modulo1/rend-condicional-form/src/components/Etapa1.js
+++ modulo1/rend-condicional-form/src/components/Etapa1.js
@@ -9,7 +9,7 @@ const Etapa1 = () => {
             <Pergunta questao={'1. Qual o seu nome?'} />
             <Pergunta questao={'2. Qual sua idade?'} />
             <Pergunta questao={'3. Qual seu email?'} />
-            <Pergunta questao={'4. Qual a sua escolaridade?'} opcoes={['Ensino médio incompleto', 'Ensino médio completo','Ensino superior incompleto','Ensino superior completo']} tipo = {'opcoes'}/>
+            <Pergunta questao={'4. Qual a sua escolaridade?'} opcoes={['Ensino médio incompleto','Ensino médio completo','Ensino superior incompleto','Ensino superior completo']} tipo = {'opcoes'}/>
         </div>
     )
 }
--- modulo1/rend-condicional-form/src/components/Pergunta.js
+++ modulo1/rend-condicional-form/src/components/Pergunta.js
@@ -2,21 +2,19 @@ import React from "react";
 
 const Pergunta = (props) => {
 
-    const dados = [props.opcoes]
-    
-    console.log(props.opcoes);
-    
-    const dadosGenericos = ['1° Opção', '2 Opção', '3° Opção', '4° Opção']
+    const dados = props.opcoes
 
-    const listaDeOpcoes = dadosGenericos.map(opcao => {
-        return <option key={opcao}>{opcao}</option>
-    })
+    console.log(props.opcoes);
+
+    const dadosGenericos = ['1° Opção', '2° Opção', '3° Opção', '4° Opção']
+
+    // const listaDeOpcoes = dados.map((opcao) => { 
+    //     return <option>{opcao}</option>
+    // })
 
     const renderPergunta = () => {
         if (props.tipo === 'opcoes') {
-            return <select>
-                {listaDeOpcoes}
-            </select>
+            return <select> {dados.map((opcao) => { return <option>{opcao}</option>})} </select>
         } else {
             return <input />
         }
