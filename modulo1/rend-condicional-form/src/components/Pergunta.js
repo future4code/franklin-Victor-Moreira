28a6e3d1a9c2cedd16b87ef9b09fd4503abc6df4
Victor Ramon <victoramonfm@gmail.com>
2022-06-09 15:44:44 -0300

    LabeForm

--- modulo1/rend-condicional-form/src/components/Pergunta.js
+++ modulo1/rend-condicional-form/src/components/Pergunta.js
@@ -4,17 +4,13 @@ const Pergunta = (props) => {
 
     const dados = props.opcoes
 
-    console.log(props.opcoes);
+    // const listaDeOpcoes = dados.map((opcao) => { return <option>{opcao}</option>})
 
-    const dadosGenericos = ['1° Opção', '2° Opção', '3° Opção', '4° Opção']
-
-    // const listaDeOpcoes = dados.map((opcao) => { 
-    //     return <option>{opcao}</option>
-    // })
+    // console.log(`listaDeOpcoes: ${listaDeOpcoes}`);
 
     const renderPergunta = () => {
         if (props.tipo === 'opcoes') {
-            return <select> {dados.map((opcao) => { return <option>{opcao}</option>})} </select>
+            return <select> {dados.map((opcao) => { return <option key={opcao}>{opcao}</option>})} </select>
         } else {
             return <input />
         }
