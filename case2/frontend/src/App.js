f4d6d04ff7cbfad2c2941168555ab500f4d02d62
Victor Ramon Firmo Moreira <victor.moreira@grupoboticario.com.br>
2022-10-14 12:03:49 -0300

    --amend

--- case2/frontend/src/App.js
+++ case2/frontend/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [contributors, setContributors] = useState([])
+
+  const body = {
+    "firstName": "Shikamaru",
+    "lastName": "Nara",
+    "participation": 4
+  }
+
+  const getContributors = () => {
+    axios.post('http://localhost:3003/contributors', body)
+      .then((response) => {
+        setContributors(response.data.contributorsDb)
+        console.log(response.data.contributorsDb);
+      })
+      .catch((error) => {
+        console.log(error.response)
+      })
+  }
+
+  useEffect(getContributors, [])
+
+  console.log(`Contributors: ${contributors}`);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" /><input type="text" /><input type="number" />
+      <button onClick={getContributors} >Send</button>
+
+      <h1>Data</h1>
+
+      <p>Commodo laboris exercitation excepteur excepteur duis veniam minim consequat exercitation aliquip excepteur et pariatur.</p>
+      {/* 
+      {
+        <table>
+          <tr>
+            <th></th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Participation</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Minato</td>
+            <td>Namikazi</td>
+            <td>50%</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Naruto</td>
+            <td>Uzumaki</td>
+            <td>30%</td>
+          </tr>
+        </table>
+      } */}
+      {/* 
+      {
+        contributors.map((contributor) => {
+          return (
+            <li key={contributor.key}>
+              {contributor.lastName}
+            </li>
+          )
+        })
+      } */}
+
     </div>
-  );
+  )
+
 }
 
 export default App;
